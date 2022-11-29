e506508f80515b9b10b5ced9e93f6cba47cc3775
fengrui tian <3134648545@qq.com>
2022-11-29 09:05:34 +0800

    增加了hpp文件

---
+++ 用户界面与路径分析/用户界面与路径分析.hpp
@@ -0,0 +1,3 @@
+1  __declspec(dllexport)void receive(std::vector<std::pair<int, int> >Re)
+2  __declspec(dllexport)std::vector<std::pair<int, int> > Sentpoints(HINSTANCE hInstance, int       nCmdShow)
+#pragma comment(lib,"用户界面与路径分析.lib")

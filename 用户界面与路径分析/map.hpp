d57f9f7ce44fae228847a0048aade6bece1ad83d
fengrui tian <3134648545@qq.com>
2022-11-29 15:59:23 +0800

    map.hpp

---
+++ 用户界面与路径分析/map.hpp
@@ -0,0 +1,3 @@
+1  __declspec(dllexport)void receive(std::vector<std::pair<int, int> >Re);
+2  __declspec(dllexport)std::vector<std::pair<int, int> > Sentpoints(HINSTANCE hInstance, int nCmdShow);
+#pragma comment(lib,"map.lib")
